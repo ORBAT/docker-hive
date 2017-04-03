8630fd893abbd59ec5d2e462c36a8fabee98b75f
Tom Eklöf <tome@unity3d.com>
2017-04-03 13:39:12 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker Hive metastore
+
+Based on [bde2020/hive](https://hub.docker.com/r/bde2020/hive/)
---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function addProperty() {
+  local path=$1
+  local name=$2
+  local value=$3
+
+  local entry="<property><name>$name</name><value>${value}</value></property>"
+  local escapedEntry=$(echo $entry | sed 's/\//\\\//g')
+  sed -i "/<\/configuration>/ s/.*/${escapedEntry}\n&/" $path
+}
+
+function configure() {
+    local module=$1
+    local envPrefix=$2
+
+    local var
+    local value
+
+    echo "Configuring $module"
+    for c in `printenv | perl -sne 'print "$1 " if m/^${envPrefix}_(.+?)=.*/' -- -envPrefix=$envPrefix`; do
+        name=`echo ${c} | perl -pe 's/___/-/g; s/__/_/g; s/_/./g'`
+        var="${envPrefix}_${c}"
+        value=${!var}
+        echo " - Setting $name=$value"
+        addProperty /opt/hive/conf/$module-site.xml $name "$value"
+    done
+}
+
+configure hive HIVE_SITE_CONF
+exec $@
